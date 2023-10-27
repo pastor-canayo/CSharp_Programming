5ba39f528b5ce171f8ba7c5007a28fbde7469018
Pastor Canayo <pastorcanayo@hotmail.com>
2023-10-27 16:23:39 -0600

    Add project files.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("What is your name: ");
+string name = Console.ReadLine();
+
+var currentDate = DateTime.Now;
+
+Console.WriteLine("Your name is " + name + " and the current date is: " + currentDate);
+
+Random random = new Random();
+for (int i = 0; i < 10; i++)
+    Console.WriteLine(random.Next(1, 100));
